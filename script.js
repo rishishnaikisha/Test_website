b0552dc9d5108eee20327d8af09541aca8409e02
rishishnaikisha <rishish.naik-ext@sadhguru.org>
2025-10-28 12:11:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("helloBtn").addEventListener("click", function() {
+  const msg = document.getElementById("message");
+  msg.textContent = "Hello there! 👋 You just clicked the button!";
+});
